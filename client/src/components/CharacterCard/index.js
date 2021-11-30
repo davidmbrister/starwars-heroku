fc1e529b660f2810872502ed6cbb68060f561756
davidmbrister <davidmurdockbrister@gmail.com>
2021-11-30 16:07:25 -0500

    changed fetch call to assume / and rebuilt react

--- client/src/components/CharacterCard/index.js
+++ client/src/components/CharacterCard/index.js
@@ -28,7 +28,7 @@ const CharacterCard = ({ characterId }) => {
     <div>
       {!characterId ? null : (
         <Fetch
-          uri={`/api/starWarsAPI/${characterId}`}
+          uri={`/starWarsAPI/${characterId}`}
           renderSuccess={CharacterTemplate}
         />
       )}
